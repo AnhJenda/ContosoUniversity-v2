3992dd54f0ab0ecaf9c0fc83f518f93162c54948
AnhJenda <100700718+TinyTots-ePj@users.noreply.github.com>
2022-12-11 23:01:14 +0700

    Initial commit

---
+++ Data/ContosoUniversity_v2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity_v2.Models;
+
+namespace ContosoUniversity_v2.Data
+{
+    public class ContosoUniversity_v2Context : DbContext
+    {
+        public ContosoUniversity_v2Context (DbContextOptions<ContosoUniversity_v2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContosoUniversity_v2.Models.Student> Student { get; set; } = default!;
+        public DbSet<ContosoUniversity_v2.Models.Course>  Course { get; set; } = default!;
+        public DbSet<ContosoUniversity_v2.Models.Enrollment>  Enrollment { get; set; } = default!;
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using ContosoUniversity_v2.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity_v2.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
