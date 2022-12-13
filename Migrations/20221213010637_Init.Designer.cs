577eed19836a186a4c5a5f2959ed112692e983d2
AnhJenda <100700718+TinyTots-ePj@users.noreply.github.com>
2022-12-13 09:58:22 +0700

    update

--- Migrations/20221213010637_Init.Designer.cs
+++ Migrations/20221213010637_Init.Designer.cs
@@ -4,16 +4,19 @@ using ContosoUniversity_v2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace ContosoUniversity_v2.Migrations
+namespace ContosoUniversityv2.Migrations
 {
     [DbContext(typeof(ContosoUniversity_v2Context))]
-    partial class ContosoUniversityv2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221213010637_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
